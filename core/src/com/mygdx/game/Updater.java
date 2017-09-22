d2962ea42d3a8d53eefa7214a448f6f1b64e8d65
Markus Lindberg <markus.lindberg073@gmail.com>
2017-09-22 18:03:59 +0300

    Template for the movement logic for the character

--- core/src/com/mygdx/game/Updater.java
+++ core/src/com/mygdx/game/Updater.java
@@ -2,6 +2,8 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -13,13 +15,28 @@ public class Updater extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("1Currency.png");
+	
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		// Movement logic template for the character
+		if(Gdx.input.isKeyPressed(Keys.W)){
+			
+		}else if(Gdx.input.isKeyPressed(Keys.A)){
+			
+		}else if(Gdx.input.isKeyPressed(Keys.S)){
+			
+		}else if(Gdx.input.isKeyPressed(Keys.D)){
+			
+		}
+		
+		
+		
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
