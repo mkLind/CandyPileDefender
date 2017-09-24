7c2e297749460676da2ec6d066d2a8fa64c2e0bd
Markus Lindberg <markus.lindberg073@gmail.com>
2017-09-24 17:24:05 +0300

    Animator constructor now splits a spritesheet into individual animations

--- core/src/com/mygdx/game/Animator.java
+++ core/src/com/mygdx/game/Animator.java
@@ -14,6 +14,21 @@ private TextureRegion[][] splitImages;
 private ArrayList<TextureRegion[]> animatedFrames;
 
 public Animator(int row, int column, float frametime, Texture spritesheet){
+	this.spritesheet = spritesheet;
+	this.row = row;
+	this.column = column;
+	this.frametime = frametime;
+	animatedFrames = new ArrayList<>();
+	// Split images of the spriteSheet into a matrix
+	splitImages = TextureRegion.split(spritesheet, spritesheet.getWidth()/this.column,spritesheet.getHeight()/this.row );
+	for(int i = 0; i<row; i++){
+		TextureRegion[] oneAnimation = new TextureRegion[column]; 
+		for(int j = 0; j<column; j++){
+			oneAnimation[j] = splitImages[i][j];
+			
+		}
+		animatedFrames.add(oneAnimation);
+	}
 	
 }
 
