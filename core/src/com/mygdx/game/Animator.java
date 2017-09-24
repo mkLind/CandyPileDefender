f8973b8def562281411d35d943dc5d441f867364
Markus Lindberg <markus.lindberg073@gmail.com>
2017-09-24 17:11:58 +0300

    Added some fields for animator class

--- core/src/com/mygdx/game/Animator.java
+++ core/src/com/mygdx/game/Animator.java
@@ -1,5 +1,21 @@
 package com.mygdx.game;
 
+import java.util.ArrayList;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 public class Animator {
+private int row;
+private int column;
+private float frametime;
+private Texture spritesheet;
+private TextureRegion[][] splitImages;
+private ArrayList<TextureRegion[]> animatedFrames;
+
+public Animator(int row, int column, float frametime, Texture spritesheet){
+	
+}
+
 
 }
--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
+ // sprite.setRegion(animation.getKeyFrame(time));
 
 public class Player extends Sprite {
 
