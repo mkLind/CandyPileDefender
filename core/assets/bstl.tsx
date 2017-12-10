8a43c5105d1c3fbdcbe3d7b4f317d1ef8bba259e
Markus Lindberg <markus.lindberg073@gmail.com>
2017-12-10 16:47:12 +0200

    AVOIDING CONFLICT

---
+++ core/assets/bstl.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="BASE_TILES" tilewidth="30" tileheight="30" tilecount="36" columns="6">
+ <image source="NewTiles.png" width="180" height="180"/>
+</tileset>
